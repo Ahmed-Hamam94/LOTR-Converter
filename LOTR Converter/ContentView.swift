b166e014afbcaa5eff15eed53c36757b062ff9f3
Ahmed Hamam <ahmed.hamam@areebgroup.com>
2025-02-16 19:07:46 +0200

    Initial Commit

---
+++ LOTR Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTR Converter
+//
+//  Created by Ahmed Hamam on 16/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
